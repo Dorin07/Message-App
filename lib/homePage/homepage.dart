aebba3e78022732910acd7b9c4518714a276370e
Dorin07 <47871786+Dorin07@users.noreply.github.com>
2021-01-06 19:52:24 +0200

    Added a listview, where you can like or not a message.

--- lib/homePage/homepage.dart
+++ lib/homePage/homepage.dart
@@ -1,16 +1,34 @@
 import 'package:flutter/material.dart';
 
+class Post {
+  String body;
+  String author;
+  int likes = 0;
+  bool userLiked = false;
+
+  Post(this.body, this.author);
+
+  void likePost() {
+    this.userLiked = !this.userLiked;
+    if (this.userLiked) {
+      this.likes += 1;
+    } else {
+      this.likes -= 1;
+    }
+  }
+}
+
 class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String text = "";
+  List<Post> posts = [];
 
-  void changeText(text) {
+  void newPost(text) {
     this.setState(() {
-      this.text = text;
+      posts.add(Post(text, "Dorin"));
     });
   }
 
@@ -21,7 +39,10 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text('Message App'),
       ),
       body: Column(
-        children: [TextInputWidget(this.changeText), Text(this.text)],
+        children: [
+          Expanded(child: PostList(this.posts)),
+          TextInputWidget(this.newPost)
+        ],
       ),
     );
   }
@@ -48,6 +69,7 @@ class _TextInputWidgetState extends State<TextInputWidget> {
   void click() {
     widget.callback(controller.text);
     controller.clear();
+    FocusScope.of(context).unfocus();
   }
 
   @override
@@ -63,5 +85,59 @@ class _TextInputWidgetState extends State<TextInputWidget> {
               tooltip: "Post message!",
               onPressed: this.click),
         ));
+  }
+}
+
+class PostList extends StatefulWidget {
+  final List<Post> listItems;
+
+  PostList(this.listItems);
+
+  @override
+  _PostListState createState() => _PostListState();
+}
+
+class _PostListState extends State<PostList> {
+  void like(Function callBack) {
+    this.setState(() {
+      callBack();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: this.widget.listItems.length,
+      itemBuilder: (context, index) {
+        var post = this.widget.listItems[index];
+        return Card(
+          child: Row(
+            children: [
+              Expanded(
+                  child: ListTile(
+                title: Text(post.body),
+                subtitle: Text(post.author),
+              )),
+              Row(
+                children: [
+                  Container(
+                    child: Text(
+                      post.likes.toString(),
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.thumb_up),
+                    onPressed: () => like(post.likePost),
+                    color: post.userLiked ? Colors.green : Colors.black,
+                  )
+                ],
+              )
+            ],
+          ),
+        );
+      },
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Mess App',
+      title: 'Message App',
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
         visualDensity: VisualDensity.adaptivePlatformDensity,
